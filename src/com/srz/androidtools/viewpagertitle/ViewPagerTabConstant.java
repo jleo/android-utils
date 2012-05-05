8375a68d3cb2a1d6e1309d9dfb692b7b56925ac4
jleo <ggyyleo@gmail.com>
2012-05-05 15:37:36 +0800

    转移

---
+++ src/com/srz/androidtools/viewpagertitle/ViewPagerTabConstant.java
@@ -0,0 +1,14 @@
+package com.srz.androidtools.viewpagertitle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jleo
+ * Date: 12-5-5
+ * Time: 上午9:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class ViewPagerTabConstant {
+    public static final int PAGER_UNDERLINE_COLOR = 0xff93d813;
+    public static final int PAGER_CENTER_TEXT_COLOR = 0xFFFFFFFF;
+    public static final int PAGER_SIDE_TEXT_COLOR = 0xFF646470;
+}
---
+++ src/com/srz/androidtools/viewpagertitle/ViewPagerTabProvider.java
@@ -0,0 +1,5 @@
+package com.srz.androidtools.viewpagertitle;
+
+public interface ViewPagerTabProvider {
+  public String getTitle(int position);
+}
