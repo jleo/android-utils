6f0291d003dc7082855b396efa73985898fcf79c
rzshang@gmail.com <rzshang@gmail.com>
2012-05-05 22:34:44 +0800

    Signed-off-by: rzshang@gmail.com <rzshang@gmail.com>

--- src/com/srz/androidtools/util/TimeTools.java
+++ src/com/srz/androidtools/util/TimeTools.java
@@ -6,11 +6,12 @@ import java.util.Date;
 import java.util.TimeZone;
 
 public class TimeTools {
+    public static String TIMEFORMATSTRING =  "yyyy MM-dd HH:mm";
     
     //时间输入时均为GMT+8的时间，输出时按手机local时区
     public static String handleTimeWithTimezone(String timeString) {
         //  System.out.println(timeString);
-          SimpleDateFormat df = new SimpleDateFormat("yyyy MM-dd HH:mm");
+          SimpleDateFormat df = new SimpleDateFormat(TIMEFORMATSTRING);
           df.setTimeZone(TimeZone.getTimeZone("GMT+8"));
           try {
               Date date = df.parse(timeString);
@@ -20,5 +21,14 @@ public class TimeTools {
               e.printStackTrace();
               return null;
           }  
+      }
+    
+    //时间输入时为date ，输出时按手机local时区
+    public static String handleTimeWithTimezone(Date date) {
+        //  System.out.println(timeString);
+          SimpleDateFormat df = new SimpleDateFormat(TIMEFORMATSTRING);
+          df.setTimeZone(TimeZone.getDefault());
+          return df.format(date);
+          
       }
 }
